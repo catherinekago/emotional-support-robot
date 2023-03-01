f04df9edd404beaf56cf338535d2a3459df64dbc
kluna95 <karenalessandraluna@gmail.com>
2023-03-01 10:47:51 +0100

    choreography

--- catkin_ws/src/database_communication/scripts/receiveMessageFromAndroid.py
+++ catkin_ws/src/database_communication/scripts/receiveMessageFromAndroid.py
@@ -1,7 +1,7 @@
 # info https://towardsdatascience.com/essentials-for-working-with-firestore-in-python-372f859851f7
 
 
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
 from pymycobot import MyCobot
@@ -24,25 +24,26 @@ import time
 
 import os
 
-
 # Use the private key file of the service account directly.
 ## TODO: copy PATH (ABSOLUTE, not relative of the firebase credential file to cred)
-cred = credentials.Certificate("/home/ubunutu/Desktop/emotional-support-robot/emotional-support-robot/catkin_ws/src/database_communication/scripts/firebase-credentials/emotional-support-robot-firebase-adminsdk-pvqeh-82d0908a80.json")
+cred = credentials.Certificate(
+    "/home/ubunutu/Desktop/emotional-support-robot/emotional-support-robot/catkin_ws/src/database_communication/scripts/firebase-credentials/emotional-support-robot-firebase-adminsdk-pvqeh-82d0908a80.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # Initiate a MyCobot object
 
-#----Local connection (if there is an error, try ttyACM1):
-#mc = MyCobot('/dev/ttyACM0', 115200)
+# ----Local connection (if there is an error, try ttyACM1):
+# mc = MyCobot('/dev/ttyACM0', 115200)
 
-#----WIFI:
+# ----WIFI:
 mc = MyCobotSocket("192.168.1.106", 9000)
 
 # Create Firebase Firestore listener
 
 # Create an Event for notifying main thread.
 callback_done = threading.Event()
+
 
 # Create a callback on_snapshot function to capture changes
 def on_snapshot(doc_snapshot, changes, read_time):
@@ -55,10 +56,10 @@ def on_snapshot(doc_snapshot, changes, read_time):
     # set global variable to trigger publishing received emotion
     global body
     body = bodyFromSnapshot
-    if(body == "SNAKE"):
+    if (body == "SNAKE"):
         print("snakey")
-        #idle state == snake
-        mc.set_color(240,240,240)
+        # idle state == snake
+        mc.set_color(240, 240, 240)
         mc.send_angles([88.68, -138.51, 155.65, -128.05, -9.93, -15.29], 50)
     elif (body == "WAKEWORD"):
         print("Body is wakeword")
@@ -68,21 +69,21 @@ def on_snapshot(doc_snapshot, changes, read_time):
 
     callback_done.set()
 
+
 def wakeWordDetected():
-    #activate robot --> wake word by app
+    # activate robot --> wake word by app
     db.collection(u'android-robot-communication').document("MESSAGE").update({u'body': "AWAKE"})
 
     # THIS IS FOR TESTING ROBOT CODE (should be moved to actOnMessageFromAndroid eventually)
     print("Robot awakened")
-        
+
     ##listening state
     mc.send_angles([0, 0, 0, 0, 0, 0], 50)
     time.sleep(1.1)
-    mc.set_color(0,150,255)
+    mc.set_color(0, 150, 255)
 
-
-    #listening routine
-    #TODO: add more listening signs (tilting the head)
+    # listening routine
+    # TODO: add more listening signs (tilting the head)
     time.sleep(1)
     mc.send_angle(Angle.J5.value, -20, 80)
     time.sleep(0.7)
@@ -91,24 +92,24 @@ def wakeWordDetected():
     mc.send_angle(Angle.J5.value, -10, 80)
     time.sleep(4)
 
+
 def emotionDetected(emotion):
     print("Received an emotion")
 
     ##active state
-    mc.set_color(0,255,0)
+    mc.set_color(0, 255, 0)
     db.collection(u'android-robot-communication').document("MESSAGE").update({u'body': "PLAYING"})
 
-    #Reaction to emotion initiated here
-    #HAPPY_BPM
+    # Reaction to emotion initiated here
+    # HAPPY_BPM
     if (emotion == "HAPPY_109" or "HAPPY_128"):
-        #TODO Happy Dances
-        happyDance()
+        # TODO Happy Dances
+        happyDance(emotion)
         print("Happy dance")
-    elif(emotion == "ANXIOUS"):
-        #TODO Breathing exercise
+    elif (emotion == "ANXIOUS"):
+        # TODO Breathing exercise
 
         for i in range(1):
-
             mc.send_angles([-25, 38, 10, -55, 27, 0], 10)
             time.sleep(4)
 
@@ -122,17 +123,18 @@ def emotionDetected(emotion):
             time.sleep(4)
 
     else:
-        #TODO Default case?
+        # TODO Default case?
         print("Emotion: " + emotion)
-            
-    #TODO: user can interrupt routine
+
+    # TODO: user can interrupt routine
 
     ##idle state == snake
-    mc.set_color(240,240,240)
+    mc.set_color(240, 240, 240)
     mc.send_angles([88.68, -138.51, 155.65, -128.05, -9.93, -15.29], 50)
     db.collection(u'android-robot-communication').document("MESSAGE").update({u'body': "SNAKE"})
-        
+
     time.sleep(4)
+
 
 docs_ref = db.collection(u'android-robot-communication').document("MESSAGE")
 
@@ -140,32 +142,66 @@ docs_ref = db.collection(u'android-robot-communication').document("MESSAGE")
 doc_watch = docs_ref.on_snapshot(on_snapshot)
 
 
-def happyDance():
-    bpm = 220
-    while bpm > 0:
-        if bpm % 2 == 0:
-            mc.send_angles([180, 15, 10, 80, 23, 0], 90)
-            time.sleep(0.7)
-            mc.send_angles([-180, -5, -5, -60, -56, 0], 90)
-            time.sleep(0.7)
-        elif bpm % 3 == 0:
-            mc.send_angles([30, 25, 15, 25, 50, 0], 90)
-            time.sleep(0.7)
-            mc.send_angles([-20, -20, -10, -25, -50, 0], 90)
-            time.sleep(0.7)
-        elif bpm % 5 == 0:
-            mc.send_angles([20, -5, -5, -5, 10, 0], 90)
-            time.sleep(0.7)
-            mc.send_angles([-20, -20, -10, -15, -10, 0], 90)
-            time.sleep(0.7)
-        else:
-            #good
-            time.sleep(1)
-            mc.send_angles([0, 0, 0, 20, 30, 0], 50)
-            time.sleep(0.7)
-            mc.send_angles([-10, 20, 0, -45, -30, 0], 50)
-            time.sleep(0.7)
-        bpm -= 1
+def happyDance(emotion):
+    if emotion == "HAPPY_128":
+        start = time.time()
+        bpm = 90
+        while time.time() - start < 90:  # 199
+            if bpm % 7 == 0:
+                x = 3
+                while x > 0:
+                    mc.send_angles([-1.49, 115, -153.45, 30, -33.42, 137.9], 100)
+                    time.sleep(0.15)
+                    mc.send_angles([-1.49, 55, -153.45, 80, 33.42, 137.9], 100)
+                    time.sleep(0.15)
+                    x -= 1
+            elif bpm % 2 == 0:
+                mc.send_angles([50, 15, 5, 20, 40, 0], 100)
+                time.sleep(0.15)
+                mc.send_angles([-50, -15, -5, -30, -56, 0], 100)
+                time.sleep(0.15)
+            elif bpm % 5 == 0:
+                mc.send_angles([70, 25, 15, 15, 50, 0], 100)
+                time.sleep(0.15)
+                mc.send_angles([-70, -25, -15, -30, -56, 0], 100)
+                time.sleep(0.15)
+            else:
+                # good
+                time.sleep(0.35)
+                mc.send_angles([0, 0, 0, 20, 30, 0], 100)
+                time.sleep(0.15)
+                mc.send_angles([-10, 20, 0, -45, -30, 0], 100)
+                time.sleep(0.15)
+            bpm -= 1
+    else:
+        start = time.time()
+        bpm = 91
+        while time.time() - start < 91:  # 196
+            x = 4
+            if bpm % 8:
+                mc.send_angles([-1.49, 115, -153.45, 30, -33.42, 137.9], 100)
+                time.sleep(0.5)
+                mc.send_angles([-1.49, 55, -153.45, 80, 33.42, 137.9], 100)
+                time.sleep(0.5)
+            elif bpm % 7:
+                # good
+                while x > 0:
+                    time.sleep(0.35)
+                    mc.send_angles([0, 0, 0, 20, 30, 0], 100)
+                    time.sleep(0.15)
+                    mc.send_angles([-10, 20, 0, -45, -30, 0], 100)
+                    time.sleep(0.15)
+                    x -= 1
+            else:
+                while x > 0:
+                    time.sleep(0.35)
+                    mc.send_angles([20, 15, 10, 10, 20, 0], 100)
+                    time.sleep(0.15)
+                    mc.send_angles([-10, 20, -10, -25, 120, 0], 100)
+                    time.sleep(0.15)
+                    x -= 1
+            bpm -= 1
+    mc.send_angles([0, 0, 0, 0, 0, 0], 50)
     print("Happy dance")
 
 
@@ -173,25 +209,25 @@ def happyDance():
 def receiveMessageFromAndroid():
     pub = rospy.Publisher('messageFromAndroid', String, queue_size=10)
     rospy.init_node('receiveMessageFromAndroid', anonymous=True)
-    rate = rospy.Rate(3) # 1hz
+    rate = rospy.Rate(3)  # 1hz
 
     global body
     body = ""
 
     while not rospy.is_shutdown():
-        
+
         if body != "":
             rospy.loginfo("publishing body " + body)
             pub.publish(body)
-            
+
             # reset message
             body = ""
 
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
         receiveMessageFromAndroid()
     except rospy.ROSInterruptException:
         pass
-
